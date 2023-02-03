27de5cd022e5db5b7c0d8d735d0343a026946a78
stan <77831846+stanuwu@users.noreply.github.com>
2023-02-03 14:10:58 +0100

    updated serializer added support for null values added support for any enum

--- src/main/java/net/mcaurora/serialization/DeepObjectSerializer.java
+++ src/main/java/net/mcaurora/serialization/DeepObjectSerializer.java
@@ -74,10 +74,14 @@ public class DeepObjectSerializer {
             if (Modifier.isStatic(f.getModifiers())) continue;
             f.setAccessible(true);
             String nextKey = key + "." + f.getName();
-            if (f.getType().isPrimitive() || JAVA_CLASS_REGEX.matcher(f.getType().getName()).matches()) {
+            if (f.get(object) == null) {
+                jsonObject.add(nextKey, null);
+            } else if (f.getType().isPrimitive() || JAVA_CLASS_REGEX.matcher(f.getType().getName()).matches()) {
                 jsonObject.addProperty(nextKey, f.get(object).toString());
             } else if (f.getType().isArray()) {
                 jsonObject.add(nextKey, serializeArray(f.get(object), f.getType().componentType()));
+            } else if (f.getType().isEnum()) {
+                jsonObject.addProperty(nextKey, f.get(object).toString());
             } else {
                 put(f.get(object), nextKey, jsonObject);
             }
@@ -85,7 +89,7 @@ public class DeepObjectSerializer {
         return jsonObject;
     }
 
-    @SuppressWarnings("rawtypes")
+    @SuppressWarnings("rawtypes unchecked")
     @SneakyThrows
     private <T> T load(String key, Class<T> type, JsonObject jsonObject) {
         ReflectionFactory rf = ReflectionFactory.getReflectionFactory();
@@ -95,10 +99,16 @@ public class DeepObjectSerializer {
             if (Modifier.isStatic(f.getModifiers())) continue;
             f.setAccessible(true);
             String nextKey = key + "." + f.getName();
+            if (jsonObject.has(nextKey) && jsonObject.get(nextKey).isJsonNull()) {
+                f.set(tr, null);
+                return tr;
+            }
             if (f.getType().isPrimitive() || JAVA_CLASS_REGEX.matcher(f.getType().getName()).matches()) {
                 setToType(f, tr, jsonObject.get(nextKey));
             } else if (f.getType().isArray()) {
                 f.set(tr, deserializeArray(jsonObject.getAsJsonArray(nextKey), f.getType().componentType()));
+            } else if (f.getType().isEnum()) {
+                f.set(tr, Enum.valueOf((Class<Enum>) f.getType(), jsonObject.get(nextKey).getAsString()));
             } else {
                 f.set(tr, load(nextKey, f.getType(), jsonObject));
             }
@@ -129,6 +139,7 @@ public class DeepObjectSerializer {
         if (!faction.hasAddonItem(key)) return list;
         JsonArray jsonArray = new Gson().fromJson((String) faction.getAddonItem(key), JsonArray.class);
         for (JsonElement jsonElement : jsonArray) {
+            if (jsonElement.isJsonPrimitive()) continue;
             list.add(load("", type, jsonElement.getAsJsonObject()));
         }
         return list;
